c2944f4ae15e7784fb2c641be2a598e7351e38c8
JohnJosiah <josiah.john@binghamuni.edu.ng>
2023-05-08 16:42:47 +0100

    few changes

---
+++ main.py
@@ -0,0 +1,10 @@
+class Mathematics:
+
+# This function subtracts two numbers
+    def subtract(x,y):
+        return x-y;
+
+class Physics:
+# This function find the velocity
+    def velocity(d,t):
+        return d/t;
